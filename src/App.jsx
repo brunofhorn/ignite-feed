fd07fa6aa05610e2e28646fe397f021f26ac54a9
brunofhorn <b.fernandeshorn@gmail.com>
2025-03-19 23:42:29 -0300

    feat: create sidebar component and ini styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Header } from "./components/Header";
 import { Post } from "./post";
+import styles from "./App.module.css";
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
 	return (
 		<div>
 			<Header />
-			<Post />;
+			<div className={styles.wrapper}>
+				<Sidebar />
+				<main>
+					<Post />
+				</main>
+			</div>
 		</div>
-	)
+	);
 }
