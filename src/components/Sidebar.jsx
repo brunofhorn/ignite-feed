53bf11978896e4ae44335cfd13430006f005ae46
brunofhorn <b.fernandeshorn@gmail.com>
2025-03-20 00:07:59 -0300

    feat: finish sidebar and styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css';
 
 export function Sidebar(){
@@ -7,12 +8,16 @@ export function Sidebar(){
             className={styles.cover}
             src="https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/brunofhorn.png" />
                 <strong>Bruno Fernandes</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     )
