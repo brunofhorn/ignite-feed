66d3208d9eb1df8a7a1edad72ad4956f9990dfd0
brunofhorn <b.fernandeshorn@gmail.com>
2025-03-23 03:00:18 -0300

    feat: create comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css'
+
+export function Comment(){
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/brunofhorn.png" alt="" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Bruno Fernandes (você)</strong>
+                            <time title="23 de março às 09:00h" dateTime='2025-03-23 09:00:00'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns!!! </p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>33</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -46,6 +47,13 @@ export function Post() {
 				<button type="submit">Comentar</button>
 				</footer>
 			</form>
+
+			<div className={styles.commentList}>
+				<Comment />
+				<Comment />
+				<Comment />
+				<Comment />
+			</div>
 		</article>
 	);
 }
