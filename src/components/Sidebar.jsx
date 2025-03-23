961e524eccefd73138ca6af7f4886e84953ee1fd
brunofhorn <b.fernandeshorn@gmail.com>
2025-03-23 04:59:05 -0300

    feat: create avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar(){
     return (
@@ -8,7 +9,7 @@ export function Sidebar(){
             className={styles.cover}
             src="https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/brunofhorn.png" />
+                <Avatar src="https://github.com/brunofhorn.png" />
                 <strong>Bruno Fernandes</strong>
                 <span>Web Developer</span>
             </div>
